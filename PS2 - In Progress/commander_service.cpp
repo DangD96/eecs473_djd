dd4c9fe9a784a2932365b514de9617f8775a7f09
DangD96 <35606061+DangD96@users.noreply.github.com>
2018-09-27 20:34:22 -0400

    Add files via upload

--- PS2 - In Progress/commander_service.cpp
+++ PS2 - In Progress/commander_service.cpp
@@ -1,4 +1,39 @@
 //Sine commmander service
-//CMakeLists
 
+#include <ros/ros.h>
+#include <example_ros_service/CommanderServiceMsg.h>
+#include <iostream>
+#include <string>
+using namespace std;
 
+bool Commandercallback(example_ros_service::CommanderServiceMsgRequest& request, example_ros_service::CommanderServiceMsgResponse& response)
+{
+  ROS_INFO("Callback activated. Generating sinusoidal commmands.");
+  double in_amp(request.amp); //convert this to C++-class, so can use member funcs
+  double in_freq(request.freq);
+  double full_freq = 2*3.14159*in_freq;
+  //double x = 0;
+  //while(ros::ok()) {
+  for(int x=0; x<full_freq; x++) {
+    response.sin_cmd = in_amp*sin(x/in_freq);
+    //if(x >= full_freq) {
+      //x -= full_freq;
+    //}
+    //x++;
+  }
+  //}
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "commander_service");
+  ros::NodeHandle n;
+
+  ros::ServiceServer service = n.advertiseService("sinusoidal_command", Commandercallback);
+  ROS_INFO("Ready to give sinusoidal commands.");
+  
+  ros::spin();
+
+  return 0;
+}
