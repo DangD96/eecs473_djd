90cc48260799714d38ef07a6014737f59d405c16
DangD96 <35606061+DangD96@users.noreply.github.com>
2018-11-06 12:34:39 -0500

    Add files via upload

--- ps6.cpp
+++ ps6.cpp
@@ -18,6 +18,7 @@ using namespace std;
 
 bool g_take_new_snapshot=false;
 osrf_gear::LogicalCameraImage g_cam2_data;
+bool g_I_see_a_box=false;
 
 void cam2CB(const osrf_gear::LogicalCameraImage& message_holder) 
 { 
@@ -60,44 +61,45 @@ int main(int argc, char **argv) {
 	}
 	ROS_INFO("Got success response from conveyor service");
 
-	// Go until first box seen is centered
-	g_take_new_snapshot=true;
+	// Monitor logical camera 2 for shipping box
+	// Halt conveyor when logical camera 2 reports that z-value of shipping box is close to zero	
 	while(g_cam2_data.models.size()<1) { //as long as camera sees no objects, keep waiting until it sees something
 		ros::spinOnce();
 		ros::Duration(0.5).sleep();
 	}
 	ROS_INFO("I see a box");
+	g_I_see_a_box=true;
 	
-	
-	
-	
-/*
-	// Stop belt and wait for 5 seconds	
-	conveyor_srv.request.power = 0;
-    conveyor_srv.response.success=false;
-	while(!conveyor_srv.response.success) {
-		ROS_WARN("Not successful stopping conveyor yet...");
-		conveyor_client.call(conveyor_srv);
-		ros::Duration(0.5).sleep();
+	while(g_I_see_a_box) {
+		g_take_new_snapshot=true;
+		double z_value = g_cam2_data.models[0].pose.position.z; 
+		
+		if(z_value < 0 + 0.05 || z_value > 0 - 0.05) {
+			// Stop belt and wait for 5 seconds	
+			conveyor_srv.request.power = 0;
+			conveyor_srv.response.success=false;
+			while(!conveyor_srv.response.success) {
+				ROS_WARN("Not successful stopping conveyor yet...");
+				conveyor_client.call(conveyor_srv);
+				ros::Duration(0.5).sleep();
+			}
+			ROS_INFO("Successfully stopped conveyor belt");
+			ROS_INFO("Waiting for 5 seconds...");
+			ros::Duration(5).sleep();
+			
+			// Turn conveyor belt back on	
+			conveyor_srv.request.power = 100.0;
+			conveyor_srv.response.success=false;
+			while(!conveyor_srv.response.success) {
+				ROS_WARN("Not successful restarting conveyor yet...");
+				conveyor_client.call(conveyor_srv);
+				ros::Duration(0.5).sleep();
+			}
+			ROS_INFO("Successfully restarted conveyor");		
+		}
+		break; // break out of while loop once belt has been stopped for 5 seconds and restarted		
 	}
-	ROS_INFO("Successfully stopped conveyor belt");
-	ROS_INFO("Waiting for 5 seconds...");
-	ros::Duration(5).sleep();
 	
-	
-	// Turn conveyor belt back on	
-	conveyor_srv.request.power = 100.0;
-    conveyor_srv.response.success=false;
-	while(!conveyor_srv.response.success) {
-		ROS_WARN("Not successful restarting conveyor yet...");
-		conveyor_client.call(conveyor_srv);
-		ros::Duration(0.5).sleep();
-	}
-	ROS_INFO("Successfully restarted conveyor");*/
-	
-	
-	 
-
 
 	// Tell drone to pick up box
 	ros::ServiceClient drone_client = n.serviceClient<osrf_gear::DroneControl>("/ariac/drone");
@@ -105,7 +107,7 @@ int main(int argc, char **argv) {
     drone_srv.request.shipment_type = "dummy";
     drone_srv.response.success=false; 
 	while(!drone_srv.response.success) {
-		ROS_WARN("Not successful calling drone yet...");
+		//ROS_WARN("Not successful calling drone yet...");
 		drone_client.call(drone_srv);
 		ros::Duration(0.5).sleep();
 	}
@@ -114,6 +116,15 @@ int main(int argc, char **argv) {
 	
 	
 	// Stop conveyor belt
-
+	conveyor_srv.request.power = 0;
+	conveyor_srv.response.success=false;
+	while(!conveyor_srv.response.success) {
+		ROS_WARN("Not successful stopping conveyor yet...");
+		conveyor_client.call(conveyor_srv);
+		ros::Duration(0.5).sleep();
+	}
+	ROS_INFO("Successfully stopped conveyor belt - Finished");	
+	/*ROS_INFO("Finished: According to logical camera, z-value of box when conveyor halted was: "<<g_cam2_data.models[0].pose.position.z<<endl);*/ 
+	
     return 0;
 }
